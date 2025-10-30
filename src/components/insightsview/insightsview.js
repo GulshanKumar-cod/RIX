24fcacee35bcfe250ec182d6f61c3a0f3967beb9
GulshanIncubig <gulshan@incubig.org>
2025-10-30 17:40:41 +0530

    &

--- src/components/insightsview/insightsview.js
+++ src/components/insightsview/insightsview.js
@@ -161,7 +161,7 @@ company.people = defaultPeopleData[company.name] || [];
             </div>
           </div>
           <p className={styles.subtitle}>
-            Innovation intelligence — industries, technologies &people.
+            Innovation intelligence — industries, technologies & people.
           </p>
         </div>
 
